c71c5f0177bc93c6ac9d8351e4ddad6ecca2e4a3
SandovalL <125229079+leebrien@users.noreply.github.com>
2026-01-11 13:01:45 +0800

    feat: exit command

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 import sys
 
+# Build your own shell
 def main():
     while True:
+        # Prompt for user input
         sys.stdout.write("$ ")
         command = input()
         print(f"{command}: command not found")
 
+        # If input is exit
+        if command.lower() == "exit":
+            break # This will exit the loop
 
 if __name__ == "__main__":
     main()
