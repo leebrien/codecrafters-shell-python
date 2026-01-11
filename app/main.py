dd2a15e5a80fb12e6e91c7db6256e6906a9a8b6e
SandovalL <125229079+leebrien@users.noreply.github.com>
2026-01-11 13:11:06 +0800

    feat: echo command

--- app/main.py
+++ app/main.py
@@ -7,10 +7,16 @@ def main():
         sys.stdout.write("$ ")
         command = input()
 
-        # If input is exit
-        if command.lower() == "exit":
-            break # This will exit the loop
+        # if input is echo, print the input back
+        if (command.strip().startswith("echo ")):
+            echo_string = command.strip()[5:] # Since "echo " is 5 characters, we slice from index 5
+            print(echo_string)
+            continue
         
+        # If input is exit
+        if command.strip.lower() == "exit":
+            break # This will exit the loop
+
         print(f"{command}: command not found")
 
 if __name__ == "__main__":
