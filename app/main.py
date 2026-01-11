002762a566ef7de5c5016ac16291436cfbd792a2
SandovalL <125229079+leebrien@users.noreply.github.com>
2026-01-11 13:28:52 +0800

    feat: type command for builtins

--- app/main.py
+++ app/main.py
@@ -7,17 +7,35 @@ def main():
         sys.stdout.write("$ ")
         command = input()
 
-        # if input is echo, print the input back
-        if (command.strip().startswith("echo ")):
-            echo_string = command.strip()[5:] # Since "echo " is 5 characters, we slice from index 5
-            print(echo_string)
-            continue
-        
         # If input is exit
         if command.strip().lower() == "exit":
             break # This will exit the loop
 
-        print(f"{command}: command not found")
+        # if input is echo, print the input back
+        elif command.strip().startswith("echo "):
+            echo_string = command.strip()[5:] # Since "echo " is 5 characters, we slice from index 5
+            print(echo_string)
+            continue
 
+        # if input is type, call type_of_command function
+        elif command.strip().lower().startswith("type "):
+            cmd_to_check = command.strip()[5:]
+            result = type_of_command(cmd_to_check)
+            print(result.format(command=cmd_to_check))
+            break
+
+        else:
+            print(f"{command}: command not found")
+
+def type_of_command(command):
+    # bultins list
+    bultins = ["echo", "exit", "type"]
+
+    # check if command is in bultins
+    if command not in bultins:
+        return "{command}: not found"
+    
+    return "{command} is a shell builtin"
+    
 if __name__ == "__main__":
     main()
